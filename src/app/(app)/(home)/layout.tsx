ba8fd17dcaba9989501725812aaec8536d48f83a
quetrea <quetrea@hotmail.com>
2025-05-14 22:48:29 +0300

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,19 +1,49 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
+import { Category } from "@/payload-types";
 
 interface HomeLayoutProps {
   children: React.ReactNode;
 }
 
-const HomeLayout = ({ children }: HomeLayoutProps) => {
+const HomeLayout = async ({ children }: HomeLayoutProps) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+  // Kategoriler koleksiyonundan verileri alıyoruz.
+  // Sadece üst kategorileri getiriyoruz (parent'ı olmayanlar).
+  // Alt kategorileri de 1 seviye derinliğe kadar dahil ediyoruz (depth: 1).
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, // Alt kategorileri dahil et, subcategories.[0] artık "Category" tipinde olur
+    pagination: false,
+    where: {
+      parent: {
+        exists: false, // Sadece üst kategorileri getir (parent değeri olmayanlar)
+      },
+    },
+  });
+
+  // Gelen veriyi formatlıyoruz:
+  // Her kategoriye ait alt kategorileri de düzenliyoruz
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      // subcategories alanı depth: 1 olduğu için, gelen alt kategorileri doğru tipe çeviriyoruz (Category).
+      ...(doc as Category),
+      subcategories: undefined, // Alt kategorilerin alt kategorilerini temizliyoruz (daha fazla derinlik istemiyoruz)
+    })),
+  }));
   return (
     <div className="flex flex-col min-h-screen">
-      <div>
-        <Navbar />
-      </div>
-
+      <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#F4F4F0]">{children}</div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -64,7 +64,6 @@ export const Navbar = () => {
 
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
 
-
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href={"/"} className="pl-6 flex items-center">
