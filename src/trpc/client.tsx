aa63f0af86cf7d587916cfe47c027d39c4f370e7
quetrea <quetrea@hotmail.com>
2025-05-16 16:01:28 +0300

    07: trpc integration fix

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -7,10 +7,11 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+import superjson from "superjson";
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
 function getQueryClient() {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // Server: always make a new query client
     return makeQueryClient();
   }
@@ -23,15 +24,15 @@ function getQueryClient() {
 }
 function getUrl() {
   const base = (() => {
-    if (typeof window !== 'undefined') return '';
-  return process.env.NEXT_PUBLIC_APP_URL ?? 'https://localhost:3000';
+    if (typeof window !== "undefined") return "";
+    return process.env.NEXT_PUBLIC_APP_URL ?? "https://localhost:3000";
   })();
   return `${base}/api/trpc`;
 }
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
-  }>,
+  }>
 ) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
@@ -42,11 +43,11 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
-    }),
+    })
   );
   return (
     <QueryClientProvider client={queryClient}>
