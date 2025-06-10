4880feaf9bcf52ba93517fe4bb0725249b83d080
quetrea <quetrea@hotmail.com>
2025-06-10 21:51:23 +0300

    27: Subdomain Rewrites

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,19 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(slug: string) {
-  return `/tenants/${slug}`;
+  // if (process.env.NODE_ENV === "development") {
+  //   return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${slug}`;
+  // }
+
+  let protocol = "https";
+  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+  if (process.env.NODE_ENV === "development") {
+    protocol = "http";
+  }
+
+  // https://quetrea.twisell.com
+  return `${protocol}://${slug}.${domain}`;
 }
 
 export function formatCurrencyToUSD(value: number | string) {
