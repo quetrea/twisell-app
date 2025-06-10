750341f3ce75fdadab2042060586c992c91deb60
quetrea <quetrea@hotmail.com>
2025-06-10 22:58:17 +0300

    28: Deployment fix 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function generateTenantUrl(slug: string) {
   const isDevelopment = process.env.NODE_ENV === "development";
-  const isSubdomainRoutingEnabled = Boolean(
-    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!
-  );
+  const isSubdomainRoutingEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === "true";
+ 
 
   // In development or subdomain routing disabled mode, use normal routing
   if (isDevelopment || !isSubdomainRoutingEnabled) {
