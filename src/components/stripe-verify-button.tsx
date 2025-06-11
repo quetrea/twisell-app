e047ecc7573a8fe969705fdfabfb7050aeb6b1c0
quetrea <quetrea@hotmail.com>
2025-06-11 16:57:44 +0300

    fix: remove unnecessary tenant check from StripeVerifyButton component

--- src/components/stripe-verify-button.tsx
+++ src/components/stripe-verify-button.tsx
@@ -1,19 +1,8 @@
 "use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
 export const StripeVerifyButton = () => {
-  const trpc = useTRPC();
-
-  const { data: tenant, isLoading } = useQuery({
-    ...trpc.tenants.getCurrentTenant.queryOptions(),
-  });
-
-  if (isLoading || !tenant) return null;
-
-  if (tenant.stripeDetailsSubmitted) return null;
   return (
     <Link href="/stripe-verify">
       <Button>Verify account</Button>
